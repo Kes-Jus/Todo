c240c8ae9c4149e7908a43156af2628d2d79711e
merouane2000 <merouanemeh@gmail.com>
2020-08-24 21:40:56 +0100

    add Delete and buttons

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,7 +5,10 @@ const list = [{'text' : "bla bla bla"}, {'text' : "bla bla bla"}, {'text' : "bla
 class Main extends React.Component{
     constructor(props){
         super(props)
+      
+       
         this.handleAdd  = this.handleAdd.bind()
+        this.handelDelete= this.handelDelete.bind()
     }
     handleAdd = (event)=>{
         let todo = document.getElementById("todo-list")
@@ -14,16 +17,32 @@ class Main extends React.Component{
         li.appendChild(text)
         todo.appendChild(li)
     }
+ 
+    
+
+    handelDelete=()=>{
+        let todo = document.getElementById("todo-list")
+        let  li =  todo.removeChild(todo.childNodes[0])
+      //todo.remove(li)
+   // for(let i = 0 ; i<= todo.length ;i++){
+      //  todo.remove(todo[i])
+     
+    }
+
     render(){
 
         return(
             <>
+
             <button onClick={this.handleAdd}>add</button>
+            <button onClick={this.handelDelete}>Delete</button>
+           
         
            <List textlist={list}></List>
            </>
         )
     }
 }
+
 
 export default Main
